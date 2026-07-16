bef434bf90e336ea0d29c7e3bedc9cd0a764b1c7
blueheart.elf <arislam.cs28@gmail.com>
2026-07-15 21:53:58 -0400

    Added a few scenes for the user and other tests

---
+++ reusables/texture_button.gd
@@ -0,0 +1,17 @@
+extends TextureButton
+
+class_name TexButton
+
+# BASICALLY ALL OF THE CONFIG_BUTTON CODE BUT MADE FOR A TEXTURE BUTTON
+
+@export var clicky: String = "yes"
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if is_hovered(): # checking to see if the button is hovered over
+		if Input.is_action_just_pressed("yes"): # checking to see if user pressed the spacebar
+			pressed.emit() # emitting pressed signal if they have
+		
+func _gui_input(event: InputEvent) -> void: # setting a function to observe inputs (NOT from our little hover clicks above)
+	if InputEventJoypadButton or InputEventMouseButton or InputEventScreenTouch: # making sure that these are not used as actual presses
+		get_viewport().set_input_as_handled() # makes it known to ignore any clicks not from the "yes" space key being pressed
--- user/options.gd
+++ user/options.gd
@@ -11,5 +11,5 @@ func _process(delta: float) -> void:
 	pass
 
 
-func _on_options_pressed() -> void:
-	get_tree().change_scene_to_file("res://user/options.tscn")
+func _on_return_pressed() -> void:
+	get_tree().change_scene_to_file("res://home_screen.tscn")
