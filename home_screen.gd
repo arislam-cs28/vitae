4b198ed920fa9f9da92d6cecdb831152a7c43a6e
blueheart.elf <arislam.cs28@gmail.com>
2026-07-16 23:08:08 -0400

    Yeah i fixed fonts

--- home_screen.gd
+++ home_screen.gd
@@ -10,6 +10,20 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
+func _on_user_pressed() -> void:
+	get_tree().change_scene_to_file("res://user/profile.tscn")
 
 func _on_options_pressed() -> void:
 	get_tree().change_scene_to_file("res://user/options.tscn")
+
+func _on_resources_pressed() -> void:
+	get_tree().change_scene_to_file("res://user/resources/resources.tscn")
+
+func _on_study_pressed() -> void:
+	get_tree().change_scene_to_file("res://study/study.tscn")
+
+func _on_virtual_pet_pressed() -> void:
+	get_tree().change_scene_to_file("res://pet/pet.tscn")
+
+func _on_bot_pressed() -> void:
+	get_tree().change_scene_to_file("res://bot/bot.tscn")
